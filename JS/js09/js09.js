dc6b9c2de187e886dc1f0e89367cf3fd68650566
VeiyaSama <djon147@hotmail.com>
2023-05-04 16:25:50 +0200

    js09 fin

---
+++ JS/js09/js09.js
@@ -0,0 +1,62 @@
+// function produit (x,y)
+// {
+//     x*y
+
+// }
+
+// function afficheImg (imagejavascript)
+// {
+
+//     var imagejavascript = document.createElement("img");
+//     imagejavascript.src = "https://ncode.amorce.org/ressources/Pool/TB_DEV/WEB_Javascript_BASES/images/papillon.jpg";
+//     document.body.appendChild(imagejavascript);
+
+// }
+
+// afficheImg()
+// createElement
+
+
+
+
+
+var str1= ["robert" , "dupont" , "amiens" ,"80000"];
+
+
+n = prompt("Saisissez le numéro représantant l'élément voulu");
+console.log(filter);
+
+function strtok(str1 , n){
+    
+    alert((str1[n]));
+
+ }
+
+ strtok(str1,n); 
+
+
+
+
+
+
+// function id (nom, prenom){
+
+//     document.write("Votre nom est "+nom);
+//     document.write("Votre prenom est :"+prenom);
+
+// }
+
+// id ("Brognaux","Djonathane");
+
+
+// var m=[true,2,5,"d","2 m","c"];
+// alert((m[0]));
+// //ou
+// alert((m[4]));
+
+
+//  var tableau= ["robert" , "dupont" , "amiens" ,"80000"];
+
+//    var n = prompt("Saisissez le numéro représantant l'élément voulu");
+    
+//    alert((tableau[n]));
