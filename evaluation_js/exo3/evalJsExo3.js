d94f21fbc11fb7a63e76ff7e721d497920c6fb6f
VeiyaSama <djon147@hotmail.com>
2023-05-11 11:28:46 +0200

    Eval exo3 Fin

---
+++ evaluation_js/exo3/evalJsExo3.js
@@ -0,0 +1,32 @@
+
+console.log("Connected")
+
+let box;
+let prenom = prompt("Veuillez saisir un prenom");
+
+const l = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane" ,"<br>"];
+ 
+
+box = l.indexOf(prenom);
+ 
+
+document.write(prenom+"<br>");
+while(box != -1){
+    
+  
+    let prenom = prompt("Veuillez saisir un prenom");
+    box = l.indexOf(prenom);
+    l.splice(box,1);
+    document.write("Hop un prénom de trouver<br>"+prenom+"<br>");
+  
+
+
+} 
+    document.write("Ce prénom ne se trouve pas dans la liste <br>");
+    document.write("Voici la liste finale des prénoms restants a trouver : " +l);
+
+
+    
+
+
+
