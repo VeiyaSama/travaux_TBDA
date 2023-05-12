6c911a55f60635dbfdd59f9bb26242d97dd37e0c
VeiyaSama <djon147@hotmail.com>
2023-05-12 09:27:58 +0200

    Suite eval5

---
+++ evaluation_js/exo5/script.js
@@ -0,0 +1,79 @@
+console.log("Connected")
+
+
+
+let nom = document.getElementById("nom").value
+let prenom = document.getElementById("prenom").value
+let date = document.getElementById("date").value
+let codeP = document.getElementById("codeP").value
+let adrs = document.getElementById("adrs").value
+let ville = document.getElementById("ville").value
+let email = document.getElementById("email").value
+let nomError = document.getElementById("nomError").value
+let prenomError = document.getElementById("prenomError").value
+let dateError = document.getElementById("dateError").value
+let codePError = document.getElementById("codePError").value
+let adrsError = document.getElementById("adrsError").value
+let villeError = document.getElementById("villeError").value
+let emailError = document.getElementById("emailError").value
+
+
+
+if (nom === ""){
+    nomError.innerHTML = "veuillez entrez votre nom";
+    nomError.style.color="red"; }
+
+
+    if (prenom === ""){
+        prenomError.innerHTML = "veuillez entrez votre prénom";
+        prenomError.style.color="red"; }
+
+
+
+
+        if (date === ""){
+            dateError.innerHTML = "veuillez entrez votre date de naissance";
+            dateEerror.style.color="red";}
+
+
+
+            if (codeP === ""){
+                codePError.innerHTML = "veuillez entrez votre code Postale";
+                codePError.style.color="red";}
+
+
+
+                if (adrs === ""){
+                    adrsError.innerHTML = "veuillez entrez votre adresse";
+                    adrsError.style.color="red";}
+
+
+                    if (ville === ""){
+                        villeError.innerHTML = "veuillez entrez votre ville";
+                        villeError.style.color="red";}
+
+
+
+                        if (email === ""){
+                            emailError.innerHTML = "veuillez entrez votre email";
+                            emailError.style.color="red";}
+
+
+                            if (nom === ""){
+                                nom_error.innerHTML = "veuillez entrez votre nom";
+                                nom_error.style.color="red";}
+
+
+
+
+                                if (nom === ""){
+                                    nom_error.innerHTML = "veuillez entrez votre nom";
+                                    nom_error.style.color="red";}
+
+
+
+
+
+                                    if (nom === ""){
+                                        nom_error.innerHTML = "veuillez entrez votre nom";
+                                        nom_error.style.color="red";}
